2dfce743d39de716eb467dcb3b4e37dd0d851181
Istam0808 <mamadaliyevistam@gmail.com>
2023-10-17 19:58:29 +0500

    lessons

---
+++ Lessons/8lesson.py
@@ -0,0 +1,58 @@
+# import time
+# start_time = time.time()
+# for i in range(10):
+#     for i2 in range(10):
+#         print(i2)
+# end_time = time.time()
+# a = "Taken time:", round(end_time - start_time, 10)
+# print(a)
+
+#-------------------------------------------------------------------------------
+
+# itr1 = ['a','b','c']
+# itr2 = [1,2,3]
+# itr3 = ['x', 'y','z']
+# zipped_itr = zip(itr1,itr2,itr3)
+# # print(list(zipped_itr))
+# for first,second,third in zipped_itr:
+#     print(first,second,third)
+
+#-------------------------------------------------------------------------------
+
+# import random 
+# letters = "abcdefghijklmnopqrstuvwxyz"
+# letters_ru = "абвгдеёжз"
+# numbers = "1234567890"
+# symbols = "!@#$%^&()_+<>?/;:"
+# total_sybols_for_password = 20
+
+# everything_includes = letters + letters_ru + numbers + symbols
+# created_password = ""
+
+# for i in range(total_sybols_for_password):
+#     random_int = random.randint(0, len(everything_includes)-1)
+#     created_password += everything_includes[random_int]
+# print(created_password)
+
+#-------------------------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
