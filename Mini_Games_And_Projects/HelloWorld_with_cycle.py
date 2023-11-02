678b411b330c7086279ebd33330b67e51f95ba2c
Istam0808 <mamadaliyevistam@gmail.com>
2023-11-02 13:54:20 +0500

    HelloWorld

---
+++ Mini_Games_And_Projects/HelloWorld_with_cycle.py
@@ -0,0 +1,18 @@
+import time
+
+def printCharacter(text = "Hello World", interval = 100):
+    alp = "1234567890!@#$%^&*()_+-=QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm "
+    i = 0
+    result = ""
+    while True:
+        print(result + alp[i])
+        if alp[i] == text[len(result)]:
+            result += alp[i]
+        i += 1
+        if i == len(alp):
+            i = 0
+        if len(result) == len(text):
+            break
+        time.sleep(interval / 500)
+
+printCharacter("Hello World", 10)
