173980830a0d91bc7725ac51a64fc22b5b93a57b
Istam0808 <mamadaliyevistam@gmail.com>
2023-11-13 23:44:46 +0500

    LessonsAll

--- Lessons/17lesson.py
+++ Lessons/17lesson.py
@@ -63,9 +63,35 @@
 
 #========================================================================================
 
-def check_input(fn):
-    def wrapper(*args, **kwargs):
-        for i in args:
-            if not i.isalnum():
-                raise Exception("Invalid value")
-        return fn(*args,**kwargs)
+# defining a decorator
+# def hello_decorator(func):                                              #2 STEP
+ 
+#     # inner1 is a Wrapper function in 
+#     # which the argument is called
+     
+#     # inner function can access the outer local
+#     # functions like in this case "func"
+#     def inner1():                                                       #3 STEP
+#         print("Hello, this is before function execution")
+ 
+#         # calling the actual function now
+#         # inside the wrapper function.
+#         func()
+ 
+#         print("This is after function execution")
+#     return inner1                                                       #4 STEP
+ 
+ 
+# # defining a function, to be called inside wrapper
+# def function_to_be_used():
+#     print("This is inside the function !!")
+ 
+ 
+# # passing 'function_to_be_used' inside the
+# # decorator to control its behaviour
+# function_to_be_used = hello_decorator(function_to_be_used)              #1 STEP
+ 
+ 
+# # calling the function
+# function_to_be_used()                                                   #5 STEP
+
