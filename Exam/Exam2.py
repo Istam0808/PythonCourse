33728c412065cca9b54b5337c2b17a31ed7dbff9
Istam0808 <mamadaliyevistam@gmail.com>
2023-11-14 19:40:00 +0500

    Exam

--- Exam/Exam2.py
+++ Exam/Exam2.py
@@ -3,19 +3,20 @@
 # 1. Create a function that finds n number of prime numbers.
 # RU: Создайте функцию, которая находит n количество простых чисел.
 
-# 2. Create a function that counts how much time that 
+# 2. Create a function that counts how much time that
 # function takes to execute.
-# RU: Создайте функцию, которая подсчитывает, сколько 
+# RU: Создайте функцию, которая подсчитывает, сколько
 # времени занимает выполнение этой функции.
 
-# 3. Create a class that takes this function as a method 
+# 3. Create a class that takes this function as a method
 # and returns the execution time. Then, create an object
 # and call the method.
 # RU:   Создайте класс, который принимает эту функцию как метод.
-        # и возвращает время выполнения. Затем создайте объект
-        # и вызовем метод.
+# и возвращает время выполнения. Затем создайте объект
+# и вызовем метод.
 
 import time
+
 
 def calc_time(func):
     def wrapper(*args, **kwargs):
@@ -23,9 +24,10 @@ def calc_time(func):
         result = func(*args, **kwargs)
         end_time = time.time()
         total_time = end_time - start_time
-        print(f"the f:{func.__name__} take {round(total_time,5)} seconds")
+        print(f"the f:{func.__name__} take {round(total_time, 5)} seconds")
         return result
     return wrapper
+
 
 @calc_time
 def prime_nums(n):
@@ -42,10 +44,12 @@ def prime_nums(n):
         num += 1
     return prime_numbers
 
+
 @calc_time
 def calculate_time(function, *args):
     result = function(*args)
     return result
+
 
 nums_primes_before = 10000
 print(prime_nums(nums_primes_before))
@@ -56,7 +60,7 @@ result = calculate_time(prime_nums, nums_primes_before)
 class Timer:
     def __init__(self, func):
         self.func = func
-    
+
     def f(self, *args):
         start_time = time.time()
         result = self.func(*args)
@@ -64,7 +68,7 @@ class Timer:
         finish_time = end_time - start_time
         return result, finish_time
 
+
 timer = Timer(prime_nums)
 result, execution_time = timer.f(nums_primes_before)
-print(round(execution_time , 5))
-
+print(round(execution_time, 5))
